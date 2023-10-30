34a751d8eb5ba3ede2576becacb899c7d974df99
Rohit Khairmode <140692299+Rohit-Khairmode@users.noreply.github.com>
2023-10-30 18:50:41 +0530

    Stack data structure

--- Collections/StacksAndQueue/stack.java
+++ Collections/StacksAndQueue/stack.java
@@ -34,10 +34,7 @@ public class stack<T> {
     }
     private void resize(){
         Object[] temp = new Object[2 * arr.length];
-        for(int i=0;i< arr.length;i++)
-        {
-            temp[i]=arr[i];
-        }
+        System.arraycopy(arr, 0, temp, 0, arr.length);
         this.arr=temp;
     }
     private boolean isFull(){
